5ea060a201e8126385731d6594de1ea591fcb2a0
sangam <sangam.angre@gmail.com>
2017-04-19 11:44:02 +0530

    test cases created

---
+++ tests/test_countWord.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from errors import no_function_found, incorrect_output, succeed
+
+
+class TestCountWord(TestCase):
+    def test_countWord(self):
+        try:
+            from count_word import countWord
+            result = countWord('../files/testfile.txt', 'dummy')
+            self.assertEqual(2, result)
+
+            result1 = countWord('../files/testfile.txt', 'Lorem')
+            self.assertEqual(2, result1)
+            self.assertTrue(succeed("countWord"))
+        except ImportError:
+            self.assertFalse(no_function_found("countWord"))
+        except AssertionError:
+            self.assertFalse(incorrect_output())
+        except FileExistsError:
+            self.assertFalse(no_function_found("file not found"))
